44773fd45e36516cec62addd954503b6e136767e
Aidan Wastiaux <aiwastia@pks.mpg.de>
2019-09-26 19:24:42 +0200

    used as test file for C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,12 +1,25 @@
 // my first program in C++
 #include <iostream>
-#include "helloworld.h"
+#include <cmath>
+using namespace std;
 
 int main(int argc, const char** argv)
 {
-	std::cout	<< "Hello World!" << std::endl;
-	std::cout	<< "Version "
+    std::cout	<< "Hello World!" << std::endl;
+/*	std::cout	<< "Version "
 			<< helloworld_VERSIONMAJOR << "."
 			<< helloworld_VERSION_MINOR << std::endl;
+ */
+    
+    class rate_color{
+        char col;
+        int rate;
+    };
+    enum color_type {yellow,green,red,blue,black} rainbow;
+    rainbow=red;
+    int cloud=3*rainbow;
+    cout << cloud << endl << rainbow << endl;
+    
+    
 	return 0;
 }
