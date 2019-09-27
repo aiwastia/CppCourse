566516a3408414717bb401278b9110f9c355be7d
Aidan Wastiaux <aiwastia@pks.mpg.de>
2019-09-27 12:45:39 +0200

    Defining the constructors outside the class. Call other classes with parameters inside the constructor because they need parameters that are not available at compiling time. Define pointers outside the constructor to access those elements inside the constructor.

--- Vehicles.cpp
+++ Vehicles.cpp
@@ -1,6 +1,7 @@
 // Building a car and a bike based on different objects shared or not (dependencies)
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 
@@ -61,41 +62,81 @@ public:
     material_type mat_seat;
     float max_speed;
     brand_type brand_veh;
-
-    //double wheelpressure, wheelsize;
-    Vehicle(int a, int b, color_type c, material_type ms,// double wp, double ws,
-            float speed, brand_type bbr) :
-        nseat(a), nwheel(b), col(c), mat_seat(ms),// wheelpressure(wp), wheelsize(ws),
-        max_speed(speed), brand_veh(bbr) {}
-    //Wheel wheel_veh(double wheelsize, double wheelpressure);
-    //Seat seat_veh(double mat_seat, double col);
-    int price() {return // nseat*seat_veh(mat_seat,col).price +nwheel*wheel_veh(wheelsize,wheelpressure).price()
-        +brand_veh
-        +col*2+pow(max_speed,2) ;}
+    double wheelpressure, wheelsize;
+    int seatprice,wheelprice;
+    //vector<Seat*> seats;
+    
+    Vehicle(int,int,color_type,material_type,double,double,float,brand_type);
+    int price() {return seatprice+wheelprice+brand_veh+col*2+pow(max_speed/10.,2)*10 ;}
 };
+
+
+Vehicle :: Vehicle(int a, int b, color_type c, material_type ms,double wp, double ws,
+                   float speed, brand_type bbr) :
+            nseat(a), nwheel(b), col(c), mat_seat(ms),wheelpressure(wp), wheelsize(ws),
+            max_speed(speed), brand_veh(bbr) {
+                
+                Seat seat_veh(mat_seat,col);
+                Wheel wheel_veh(wheelpressure,wheelsize);
+                
+                seatprice=nseat*seat_veh.price();
+                wheelprice=nwheel*wheel_veh.price();
+                /*for (int i=0; i<nseat; i++) {
+                    seats.push_back(new Seat(mat_seat, col));
+                    seatprice+=seats[i]->price();
+                    std::cout << seatprice << std::endl;
+                }*/
+}
+
+
 
 class Car : public Vehicle {
 public:
-    Car(int ns, color_type col, material_type mat, brand_type br) :
-    Vehicle(ns,4,col,mat,//2,0.4,
-            120,br) {}
-    // SOMETHING WITH DOORS
+    int doorprice,vehprice;
+    
+    Car(int, color_type, material_type, brand_type);
+    int price() {return doorprice+vehprice;}
 };
+
+
+Car :: Car(int ns, color_type col, material_type mat, brand_type br) :
+        Vehicle(ns,4,col,mat,2,0.4,120,br) {
+            
+            Door door_car(true,true,col);
+            Vehicle veh(ns,4,col,mat,2,0.4,120,br);
+            
+            doorprice=4*door_car.price();
+            vehprice=10*veh.price();
+}
+
 
 class Bike : public Vehicle {
 public:
-    Bike(color_type col, material_type mat, brand_type br) :
-    Vehicle(1,2,col,mat,//0.2,0.6,
-            30,br) {}
-    // SOMETHING WITH PEDALS
+    int pedalprice,vehprice;
+    
+    Bike(color_type, material_type, brand_type);
+    int price() {return pedalprice+vehprice;}
 };
 
+
+Bike :: Bike(color_type col, material_type mat, brand_type br) :
+            Vehicle(1,2,col,mat,0.2,0.6,30,br) {
+                
+                Pedal pedal_bike(0.1,0.01,false);
+                Vehicle veh(1,2,col,mat,0.2,0.6,30,br);
+                
+                pedalprice=2*pedal_bike.price();
+                vehprice=veh.price();
+            }
+
 int main() {
-    Car myfirstcar(5,grey,leather,Opel);
-    Bike myfirstbike(black,plastic,Yamaha);
+    Car myfirstcar(5,yellow,leather,VW);
+    Bike myfirstbike(black,cloth,Yamaha);
     
+    cout << myfirstcar.nwheel << endl;
     cout << myfirstcar.price() << endl;
-    cout << myfirstcar. << endl;
+    cout << myfirstbike.nwheel << endl;
+    cout << myfirstbike.price() << endl;
     
     return 0;
 }
