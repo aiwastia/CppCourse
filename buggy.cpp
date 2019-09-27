78a8fc6c4c60fcf7d40d4019ff2fc87984df5561
Aidan Wastiaux <aiwastia@pks.mpg.de>
2019-09-27 12:43:14 +0200

    file to practice debugging

---
+++ buggy.cpp
@@ -0,0 +1,25 @@
+//
+// Created by bennet on 27.09.19.
+//
+#include <iostream>
+
+int div2by(int n){
+    return 2 / n;
+}
+
+int main(int argc, const char** argv){
+    int array[10] = {};
+
+    for(int i = 0;; i++){
+        array[i] = 42;
+    }
+
+    /*for(int i = 0; i < 20; i++){
+        std::cout << array[i << 1 + 5] << std::endl;
+    }*/
+
+    /*for(int i = 10; i > -10; i--) {
+        std::cout << div2by(i) << std::endl;
+    }*/
+    return 0;
+}
